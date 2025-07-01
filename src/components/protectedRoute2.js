6296fd5fbc966ad011fbd0dd542c1207d62591e6
nummad <nummad222@gmail.com>
2025-07-01 09:54:52 +0500

    comit

--- src/components/protectedRoute2.js
+++ src/components/protectedRoute2.js
@@ -1,33 +1,11 @@
-// ProtectedRoute2.js
 import { Navigate, Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 
 const ProtectedRoute2 = () => {
-    const [authorized, setAuthorized] = useState(null);
+    const user = useSelector(state => state.user.email);
+    const loggedIn = useSelector(state => state.user.loggedIn);
 
-    useEffect(() => {
-        const loadingToast = toast.loading("Verifing ...");
-        axios.get(`${process.env.REACT_APP_BACK_END}/verify-token`, {
-            withCredentials: true
-        }).then(res => {
-            if (res.data.user.email === 'nummad222@gmail.com') {
-                toast.dismiss(loadingToast);
-                setAuthorized(true);
-            } else {
-                toast.dismiss(loadingToast);
-                setAuthorized(false);
-            }
-        }).catch((err) => {
-            console.log(err);
-            toast.dismiss(loadingToast);
-            setAuthorized(false);
-        })
-    }, []);
-
-    return authorized ? <Outlet /> : <Navigate to="/" replace />;
+    return (loggedIn && user === 'nmmad222@gmail.com') ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute2;
