fd45672c81f2ce88ab06ac0f8059d5207d79e748
nummad <nummad222@gmail.com>
2025-07-01 09:28:52 +0500

    comit

--- src/components/notifications.js
+++ src/components/notifications.js
@@ -116,7 +116,6 @@ export default function Notifications() {
         return `1 ${nextMonth}`;
     }
     useEffect(() => {
-        console.log("nptofo")
         getWinner();
     }, []);
     historyData.sort((a, b) => {
