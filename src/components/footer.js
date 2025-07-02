98bb1b6ae9965ce3ac85b619dd08e669c25808ac
nummad <nummad222@gmail.com>
2025-07-02 12:03:11 +0500

    comit

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,14 @@ import Logo from '../images/logo.png';
 import { IoLogoInstagram } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import { useSelector } from 'react-redux';
 
 export default function Footer() {
     const navigate = useNavigate();
-    const user = Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null;
+    const email = useSelector(state => {
+        const email = state.user?.email;
+        return email === '' ? null : email;
+    });
     const date = new Date().getFullYear();
     return (
         <div className={style.footer}>
@@ -18,25 +22,25 @@ export default function Footer() {
                 <p style={{ marginBottom: '15px' }}>© {date} Crunchybites All Rights Reserved.</p>
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between', gap: '20px', height: '100%', margin: 'auto 0px' }}>
                     {
-                        user != null ?
-                        user.email != "nummad222@gmail.com" ?
-                        <p style={{ cursor: 'pointer', fontSize: '18px' }} onClick={() => {user == null ? navigate('/login') : navigate('/notifications')}}>Notifications</p> :
-                        <p style={{ cursor: 'pointer', fontSize: '18px' }} onClick={() => {user == null ? navigate('/login') : navigate('/settings')}}>Settings</p> :
-                        null
+                        email != null ?
+                            email != "nummad222@gmail.com" ?
+                                <p style={{ cursor: 'pointer', fontSize: '18px' }} onClick={() => { email == null ? navigate('/login') : navigate('/notifications') }}>Notifications</p> :
+                                <p style={{ cursor: 'pointer', fontSize: '18px' }} onClick={() => { email == null ? navigate('/login') : navigate('/settings') }}>Settings</p> :
+                            null
                     }
                     {
-                    user == null ?
-                        <div className={style.orderbtn} onClick={() => navigate('/login')}>
-                        Order
-                        </div> :
-                        user.email == "nummad222@gmail.com" ?
-                            <div className={style.orderbtn} onClick={() => navigate('/customers')}>
-                                Customers
-                            </div> :
-                            <div className={style.orderbtn} onClick={() => navigate('/order')}>
+                        email == null ?
+                            <div className={style.orderbtn} onClick={() => navigate('/login')}>
                                 Order
-                            </div>
-                }
+                            </div> :
+                            email == "nummad222@gmail.com" ?
+                                <div className={style.orderbtn} onClick={() => navigate('/customers')}>
+                                    Customers
+                                </div> :
+                                <div className={style.orderbtn} onClick={() => navigate('/order')}>
+                                    Order
+                                </div>
+                    }
                 </div>
             </div>
             <div className={style.smallscreen}>
@@ -44,7 +48,7 @@ export default function Footer() {
                 <p style={{ fontSize: '20px', fontWeight: 'bold', marginBottom: '10px' }}>Eat, enjoy & win</p>
                 <p style={{ margin: '10px 0px' }}>All Rights Reserved.</p>
                 <p style={{ margin: '10px 0px' }}>© {date} Crunchybites </p>
-                
+
             </div>
         </div>
     );
