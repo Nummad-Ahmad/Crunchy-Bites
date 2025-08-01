72791b90952d98f8ec848b400f40ac1508426a51
nummad <nummad222@gmail.com>
2025-08-01 18:16:53 +0500

    comit

--- src/components/order.js
+++ src/components/order.js
@@ -51,7 +51,6 @@ export default function Order() {
         var sum = (dayName == "Friday" ? inputValues.samosa * (prices.samosa - 15) : inputValues.samosa * prices.samosa) + (dayName == "Friday" ? inputValues.fries * (prices.fries - 20) : inputValues.fries * prices.fries) + (dayName == "Friday" ? inputValues.cheesyFries * (prices.cheesyFries - 30) : inputValues.cheesyFries * prices.cheesyFries) + (dayName == "Friday" ? inputValues.lemonade * (prices.lemonade - 15) : inputValues.lemonade * prices.lemonade) + (dayName == "Friday" ? inputValues.chocoMilk * (prices.chocoMilk - 30) : inputValues.chocoMilk * prices.chocoMilk);
         return sum;
     }
-
     function sendOrder() {
         if (hours > 17 || hours < 9) {
             toast.error("Order timing is 9am to 5pm");
