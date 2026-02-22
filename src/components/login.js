4511fa0f09d23184e4089677e9efa7c7c785fe62
nummad <nummad222@gmail.com>
2026-02-22 15:19:16 +0500

    fixed

--- src/components/login.js
+++ src/components/login.js
@@ -64,7 +64,7 @@ export default function Login() {
           if (response.status === 200) {
             toast.success(response.data.message || 'Account verified successfully');
             setShowVerification(false);
-              dispatch(userLoggedIn({ email: result.data.user.email, wins: result.data.user.wins }));
+              dispatch(userLoggedIn({ email: response.data.user.email, wins: response.data.user.wins }));
             navigate('/', { replace: true });
           }
         })
